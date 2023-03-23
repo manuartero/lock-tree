2f4d8863965485bd60ad2d8a45bcadea96efb357
Manu Artero Anguita <manutero.developer@gmail.com>
2023-03-23 22:59:33 +0100

    fix: restore Content Security Policy
    <meta http-equiv="Content-Security-Policy"
          content="default-src 'none';
          style-src 'self' 'unsafe-inline';
          img-src 'self' https: data: blob: vscode-remote-resource:;
          script-src 'self' 'unsafe-eval' blob:;

--- src/html-content.ts
+++ src/html-content.ts
@@ -18,6 +18,7 @@ export function getHtmlContent(
        - only allow loading images from https or from our extension directory,
        - only allow scripts that have a specific nonce.
     -->
+    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="${media["reset.css"]}" rel="stylesheet">
